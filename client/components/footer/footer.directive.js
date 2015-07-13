c27b4fc9628a1e86f77b4c66fb8dadaca0776a27
unknown <gitatme@nxsmotorsports.com>
2015-07-13 00:15:22 -0400

    initial

---
+++ client/components/footer/footer.directive.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('mbcListerApp')
+  .directive('footer', function () {
+    return {
+      templateUrl: 'components/footer/footer.html',
+      restrict: 'E',
+      link: function (scope, element) {
+        element.addClass('footer');
+      }
+    };
+  });
